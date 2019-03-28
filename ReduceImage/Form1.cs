03edcb1f78bc17bea9ad59214a51091dee119f43
Tolunay Kaplan <tolunaykpln@hotmail.com>
2019-03-29 01:34:41 +0300

    Version 0.1
    Example codes for reduce image sizes.

---
+++ ReduceImage/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ReduceImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VaryQualityLevel();
+                MessageBox.Show("Başarılı!");
+            }
+            catch
+            {
+                MessageBox.Show("Başarısız!");
+            }
+        }
+
+        private void VaryQualityLevel()
+        {
+            // Get a bitmap. The using statement ensures objects  
+            // are automatically disposed from memory after use.  
+            using (Bitmap bmp1 = new Bitmap(@"C:\Users\Tolunay\Desktop\test\testfoto.jpg"))
+            {
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                // Create an Encoder object based on the GUID  
+                // for the Quality parameter category.  
+                System.Drawing.Imaging.Encoder myEncoder =
+                    System.Drawing.Imaging.Encoder.Quality;
+
+                // Create an EncoderParameters object.  
+                // An EncoderParameters object has an array of EncoderParameter  
+                // objects. In this case, there is only one  
+                // EncoderParameter object in the array.  
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 50L);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+                bmp1.Save(@"C:\Users\Tolunay\Desktop\test\TestPhotoQualityFifty.jpg", jpgEncoder, myEncoderParameters);
+
+                myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+                bmp1.Save(@"C:\Users\Tolunay\Desktop\test\TestPhotoQualityHundred.jpg", jpgEncoder, myEncoderParameters);
+
+                // Save the bitmap as a JPG file with zero quality level compression.  
+                myEncoderParameter = new EncoderParameter(myEncoder, 0L);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+                bmp1.Save(@"C:\Users\Tolunay\Desktop\test\TestPhotoQualityZero.jpg", jpgEncoder, myEncoderParameters);
+            }
+
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
