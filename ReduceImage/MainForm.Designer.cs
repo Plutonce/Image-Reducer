edf6660abdc4c195f05916cac89445b3bd385697
Tolunay Kaplan <30895338+Plutonce@users.noreply.github.com>
2019-03-30 14:21:14 +0300

    Version 1.0
    Different Image Formats
    ---------------------------
    *.jpg
    *.jpeg
    *.png
    *.svg
    *.gif
    *.bmp
    ---------------------------
    Up to 85% percent file size reduction.

---
+++ ReduceImage/MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ReduceImage
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.SelectFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.LblFormat = new System.Windows.Forms.Label();
+            this.CBxIFormat = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(93, 52);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(121, 48);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Reduce Images";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // LblFormat
+            // 
+            this.LblFormat.AutoSize = true;
+            this.LblFormat.Location = new System.Drawing.Point(118, 9);
+            this.LblFormat.Name = "LblFormat";
+            this.LblFormat.Size = new System.Drawing.Size(71, 13);
+            this.LblFormat.TabIndex = 2;
+            this.LblFormat.Text = "Image Format";
+            // 
+            // CBxIFormat
+            // 
+            this.CBxIFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CBxIFormat.FormattingEnabled = true;
+            this.CBxIFormat.Items.AddRange(new object[] {
+            "*.jpg",
+            "*.jpeg",
+            "*.png",
+            "*.svg",
+            "*.gif",
+            "*.bmp"});
+            this.CBxIFormat.Location = new System.Drawing.Point(93, 25);
+            this.CBxIFormat.Name = "CBxIFormat";
+            this.CBxIFormat.Size = new System.Drawing.Size(121, 21);
+            this.CBxIFormat.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 114);
+            this.Controls.Add(this.CBxIFormat);
+            this.Controls.Add(this.LblFormat);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "MainForm";
+            this.Text = "Image Size Reducer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.FolderBrowserDialog SelectFolderDialog;
+        private System.Windows.Forms.Label LblFormat;
+        private System.Windows.Forms.ComboBox CBxIFormat;
+    }
+}
+
---
+++ ReduceImage/MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ReduceImage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+        public string[] filePaths;
+        public string tempPath;
+        #endregion
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (CBxIFormat.Text != "")
+                ReduceImageSize();
+            else
+                MessageBox.Show("Please select image format.", "Image Size Reducer");
+        }
+
+        private void ReduceImageSize()
+        {
+            try
+            {
+                GetFilePathsInFolder(CBxIFormat.Text);
+
+                if (filePaths.Length == 0)
+                    this.Text = "Count: " + filePaths.Length.ToString();
+
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    GetImageCount();
+
+                    tempPath = filePaths[i];
+                    Bitmap src = Image.FromFile(tempPath) as Bitmap;
+                    Rectangle cropRect = new Rectangle(0, 0, src.Width, src.Height);
+                    Bitmap target = new Bitmap(src, cropRect.Width, cropRect.Height);
+                    ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+                    System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+                    EncoderParameters myEncoderParameters = new EncoderParameters(1);
+                    EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 50L);
+                    myEncoderParameters.Param[0] = myEncoderParameter;
+                    src.Dispose();
+                    File.Delete(tempPath);
+                    target.Save(tempPath, jpgEncoder, myEncoderParameters);
+                }
+
+                MessageBox.Show("Completed. Total Reduced Image "+this.Text, "Image Size Reducer");
+
+                this.Text = "Image Size Reducer";
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Failed. Error Exception: " + Ex.ToString(), "Image Size Reducer");
+            }
+        }
+
+        private void GetImageCount()
+        {
+            this.Text = "Count: " + filePaths.Length.ToString();
+            Application.DoEvents();
+        }
+
+        private void GetFilePathsInFolder(string FileType)
+        {
+            DialogResult result = SelectFolderDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                filePaths = Directory.GetFiles(SelectFolderDialog.SelectedPath, FileType, SearchOption.TopDirectoryOnly);
+            }
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+    }
+}
